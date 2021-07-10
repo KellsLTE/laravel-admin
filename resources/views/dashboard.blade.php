313dc37dccb6ad19947dbedb2a1f73ae3bb1a625
Favour Max-Oti <maxotif@gmail.com>
2021-07-10 01:26:23 +0100

    added post model

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,36 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="mx-auto ml-4 sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-700 dark sm:rounded-lg">
-                <div class="p-6 bg-white border-gray-200 dark:bg-gray-700 dark">
-                    You're logged in!
+    <div class="py-6 m-6">
+        <div class="w-full m-2">
+            <a href="" class="p-2 m-2 bg-green-400 rounded-lg">
+                Create Post
+            </a>
+        </div>
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                <div class="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50 dark:bg-gray-600 dark:text-gray-200">
+                    <tr>
+                        <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-200">Id</th>
+                        <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-200">Title</th>
+                        <th scope="col" class="relative px-6 py-3">Edit</th>
+                    </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-700">
+                    <tr></tr>
+                    @foreach (App\Models\Post::all() as $post)
+                        <tr>
+                            <td class="px-6 py-4 whitespace-nowrap">{{ $post->id }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap">{{ $post->title }}</td>
+                            <td class="px-6 py-4 text-sm text-right">Edit Delete</td>
+                        </tr>
+                    @endforeach
+                    <!-- More items... -->
+                    </tbody>
+                </table>
+                <div class="p-2 m-2">Pagination</div>
                 </div>
             </div>
         </div>
